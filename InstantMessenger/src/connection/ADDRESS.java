15b03f2eb2d70a65d6658379101c8ff8b5d5e4dc
Rey Perez <rey.perez2@upr.edu>
2021-12-15 17:12:42 -0400

    changed server port

--- InstantMessenger/src/connection/ADDRESS.java
+++ InstantMessenger/src/connection/ADDRESS.java
@@ -2,5 +2,5 @@ package connection;
 
 public final class ADDRESS {
 	public static final String IP_ADDRESS = "20.124.210.239";
-	public static final int SERVER_PORT = 9090;
+	public static final int SERVER_PORT = 2021;
 }

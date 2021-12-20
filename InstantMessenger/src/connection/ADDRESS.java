ec09890dd2885453241d4b3eae65da1ba0529861
Rey Perez <rey.perez2@upr.edu>
2021-12-19 21:00:20 -0400

    fix ip address

--- InstantMessenger/src/connection/ADDRESS.java
+++ InstantMessenger/src/connection/ADDRESS.java
@@ -5,6 +5,6 @@ public final class ADDRESS {
 	//local address 	127.0.0.1
 	//server address 	20.124.210.239
 	
-	public static final String IP_ADDRESS = "127.0.0.1";
+	public static final String IP_ADDRESS = "20.124.210.239";
 	public static final int SERVER_PORT = 2021;
 }
